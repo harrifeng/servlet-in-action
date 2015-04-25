0b64330b56aaabd9e87ed1ff47133be85c7dcf94
harri feng <harrifeng@gmail.com>
2015-04-25 23:11:43 +0800

    can create the example, but in windows, it does not work

---
+++ src/main/java/org/hfeng/ch3/EncodingServlet.java
@@ -0,0 +1,22 @@
+package org.hfeng.ch3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/jsp/encoding")
+public class EncodingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        String name = req.getParameter("nameGet");
+        name = new String(name.getBytes("ISO-8859-1"), "UTF-8");
+        System.out.println("Get:" + name);
+    }
+}
